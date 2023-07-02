c1306913aa3339d6c267283154a766adffa30273
wyjee <wooyeon.jee@gmail.com>
2023-07-02 16:25:20 +0900

    Add: /assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../assets/styles/globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image'
-import styles from '../styles/page.module.css'
+import styles from '../assets/styles/page.module.css'
 
 export default function Home() {
   return (
