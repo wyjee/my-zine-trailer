5bc2af7d4244d0dd40aa3579f7c336c11529885b
wyjee <wooyeon.jee@gmail.com>
2023-07-02 15:19:15 +0900

    Config: gh-pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const debug = process.env.NODE_ENV !== "production";
+const repository = "https://wyjee.github.io/my-zine";
+
+const nextConfig = {
+    reactStrictMode: true,
+    assetPrefix: !debug ? `/${repository}/` : "",
+    trailingSlash: true,
+    output: 'export',
+    assetPrefix: debug? undefined : "https://wyjee.github.io/my-zine",
+    distDir: 'build',
+};
+
+module.exports = nextConfig;
