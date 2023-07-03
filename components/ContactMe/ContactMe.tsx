2962037b8d59d007b968cabcc41e7fbd4b7b8943
wyjee <wooyeon.jee@gmail.com>
2023-07-03 10:24:16 +0900

    Modify: ReadMe text

--- components/ContactMe/ContactMe.tsx
+++ components/ContactMe/ContactMe.tsx
@@ -5,18 +5,18 @@ export default function ReadMe() {
         <div className={`${styles.col}`}>
             <section className={`${styles.col} content`}>
                 <h3 className={`${styles.content_title} highlight`}>ReadMe. more?</h3>
-                <b className={'highlight'}>2022.12. - current</b>
+                <b className={'highlight'}>2022.12. ~ current.</b>
                 <b>NSPACE, remote.</b>
                 <p>쿠폰 결제 기능 개발</p>
                 <p>서비스 메인화면 UI/UX 개편 반영</p>
                 <br/>
-                <b className={'highlight'}>2021.12. - 2022.07.</b>
+                <b className={'highlight'}>2021.12. ~ 2022.07.</b>
                 <b>Studiomate, Seoul.</b>
                 <p>문자메세지 템플릿 기능 개발</p>
                 <p>자동 결제 화면 기능 개발</p>
                 <p>React, Redux 프로젝트를 Next.js SWR 서비스로 개편: 게시판 기능 개발</p>
                 <br/>
-                <b className={'highlight'}>2020.03. - 2021.12.</b>
+                <b className={'highlight'}>2020.03. ~ 2021.12.</b>
                 <b>NSPACE, Seoul.</b>
                 <p>서비스 UI/UX 개편 반영</p>
                 <p>정기결제 화면 기능 개발</p>
